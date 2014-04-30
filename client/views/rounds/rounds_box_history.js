6f6a5f0f0d1dd6ca45c82eff91627d638b0b83d3
Ope Akanji <beepted@gmail.com>
2014-04-30 19:59:05 -0400

    Ten columns per round history row

--- client/views/rounds/rounds_box_history.js
+++ client/views/rounds/rounds_box_history.js
@@ -1,5 +1,18 @@
 Template.roundsBoxHistory.helpers({
 	rounds: function() {
 		return Rounds.find();
-	}
+	},
+
+    roundRows: function() {
+        var rounds = Rounds.find();
+        var roundRows = [];
+
+        rounds.forEach(function(round){
+            var index = round.roundNum - 1
+            if(index % 10 == 0)
+                roundRows.push([]);
+            roundRows[Math.floor(index/ 10)].push(round);
+        });
+        return roundRows;
+    }
 });
